e42025552bca2e2db700c1b4fa2b74d18c194205
Ryan Mardock <ryan.mardock98@gmail.com>
2025-08-11 16:24:15 -0500

    add example config file

---
+++ app/config/users/.example_user_config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  0 => array(
+    "name"=>"Youtube",
+    "url"=>"https://youtube.com",
+    "icon-path"=>"/tile-icons/youtube-icon.svg",
+    "darkmode"=>true,
+  ),
+);
+?>
